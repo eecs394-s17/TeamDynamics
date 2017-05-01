19e31f1e9a55fd903214ee95c9766aa953e361c0
Menglei Lei <mengleilei2018@u.northwestern.edu>
2017-05-01 18:57:37 -0500

    add firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,17 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AF } from '../Pages/firebase/firebase';
+export const firebaseConfig = {
+  apiKey: "AIzaSyAKVHJkONhx8gyWjJ0Z_AzpqLA3vjR3asI",
+  authDomain: "teamdynamics-2eae2.firebaseapp.com",
+  databaseURL: "https://teamdynamics-2eae2.firebaseio.com",
+  projectId: "teamdynamics-2eae2",
+  storageBucket: "teamdynamics-2eae2.appspot.com",
+  messagingSenderId: "560129471399"
+};
+
+
 
 @NgModule({
   declarations: [
@@ -14,7 +25,9 @@ import { AppComponent } from './app.component';
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [
+    AF
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
