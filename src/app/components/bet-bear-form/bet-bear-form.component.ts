e4e12c259c8a2729a0b1c481fb38aba4d30dc750
Alex W <alexwang2017@u.northwestern.edu>
2017-05-02 03:36:19 -0500

    some styling and created the bet/bear form

--- src/app/components/bet-bear-form/bet-bear-form.component.ts
+++ src/app/components/bet-bear-form/bet-bear-form.component.ts
@@ -2,16 +2,21 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'bet-bear-form',
-  templateUrl: './bet-bear-form.component.html'
+  templateUrl: './bet-bear-form.component.html',
+  styleUrls: ['./bet-bear-form.component.scss']
 })
 export class BetBearFormComponent implements OnInit{
+  teamMembers: Array<string>;
 
-  constructor (
-    private afService: AF){}
-
-  loading: Boolean = false;
+  constructor (){
+    this.teamMembers = [
+      'Dave',
+      'Joe',
+      'Judy',
+      'Kira'
+    ];
+  }
 
   ngOnInit(): void {
-    this.loading = true;
   }
 }
