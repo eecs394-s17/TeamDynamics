3b5a67ed2f801a9ed7739e0a563f1f7a3d82bfa3
Alex W <alexwang2017@u.northwestern.edu>
2017-05-23 13:04:51 -0500

    fixed server.js

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
-
 const express = require('express');
 const path = require('path');
 const http = require('http');
 const bodyParser = require('body-parser');
-
+const csv = require('fast-csv');
 const api = require('./server/routes/api');
-
+var firebase = require('./firebase.js');
 const app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
@@ -19,9 +18,20 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
+app.all('/post', (req, res) => {
+  var count = 0;
+  req.pipe(csv())
+  .on('error',function(err){
+    console.error('error', err);
+  })
+  .on('data',function(data){
+    count ++;
+    if(count > 5 && data.length > 1) firebase.CreateActivity(data);
+  });
+  res.send('test');
+});
+
 const port = process.env.PORT || '3000';
 app.set('port', port);
-
 const server = http.createServer(app);
-
 server.listen(port, () => console.log(`API running on localhost:${port}`));
