44f325146292ab1abd4cf7573f8e051c11ba7c67
Menglei Lei <mengleilei2018@u.northwestern.edu>
2017-05-25 20:39:21 -0500

    finished csv

--- firebase.js
+++ firebase.js
@@ -17,23 +17,83 @@ exports.showvalue = function(){
   });
 };
 
-exports.CreateActivity = function(data){
-  var email = data[2];
-  email = email.replace(/\./g, '%40');
-  var db = firebase.app().database().ref('/users/' + email);
-  db.once('value').then((snap) => {
-    if(snap.val()){
-      console.log(snap.val());
+exports.CreateActivity = function(students){
+  console.log('in create activity');
+  console.log(students);
+  var teams = {};
+  for (var i = 0; i < students.length; i++) {
+    var teamKey = students[i][3];
+    if (teams.hasOwnProperty(teamKey)) {
+        teams[teamKey].push(students[i]);
+    } else {
+      teams[teamKey] = [students[i]];
     }
-    else{
-      var user = {
-        'ID' : data[0],
-        'Name' : data[1],
-        'Email' : data[2],
-        'Team' : data[3]
-      };
-      db.set(user);
+  }
+  var db = firebase.app().database().ref('/forms');
+  for (var teamKey in teams) {
+    if (teams.hasOwnProperty(teamKey)) {
+      var team = teams[teamKey];
+      for (var i = 0; i < team.length; i++) {
+        var form = {
+          'status': 0,
+          'reviewerName': team[i][1],
+          'reviewerId': emailKey(team[i][2]),
+          'individualBetBearForms': []
+        };
+        for (var j = 0; j < team.length; j++) {
+          if (i == j) {
+            continue;
+          }
+          else {
+            var individualForm = {
+              'revieweeName': team[j][1],
+              'revieweeId': emailKey(team[j][2])
+            };
+            addBetsBears(individualForm);
+            form.individualBetBearForms.push(individualForm);
+          }
+        }
+        db.push(form);
+      }
     }
-  })
-
+  }
 };
+
+exports.CreateStudents = function(data) {
+    var key = emailKey(data[2]);
+    var db = firebase.app().database().ref('/users/' + key);
+    db.once('value').then((snap) => {
+      if(snap.val()){
+        console.log(snap.val());
+      }
+      else{
+        var user = {
+          'ID' : data[0],
+          'Name' : data[1],
+          'permission': 1
+        };
+        db.set(user);
+      }
+    });
+}
+
+function emailKey(s) {
+  return encodeURIComponent(s).split('.').join('%2e');
+}
+
+function addBetsBears(form) {
+  form.bets=[]; form.bears = [];
+  for (var i = 0; i < 2; i++ ) {
+    form.bets.push({
+      'behavior': "",
+      'effect': "",
+      'thankYou': ""
+    });
+    form.bears.push({
+      'behavior': "",
+      'effect': "",
+      'alternative': "",
+      'result': ""
+    });
+  }
+}
